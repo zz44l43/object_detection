bf540333a662d5ecf85c2e43185d6284421d6256
georgeGitM <zhiz2@student.unimelb.edu.au>
2018-02-11 15:10:23 +1100

    debug

--- research/run.sh
+++ research/run.sh
@@ -18,8 +18,9 @@ eval_dir=$output_dir/eval
 
 # config文件
 config=ssd_mobilenet_v1_pets.config
+echo "config " $config " config"
 pipeline_config_path=$output_dir/$config
-
+echo "pipeline_config_path " $pipeline_config_path " pipeline_config_path"
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
 # rm -rvf $output_dir/*
